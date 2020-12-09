9b553755575d292c120ff07f9ed0c3a36f47a7a4
Tony <dongo824@naver.com>
2020-12-09 11:52:31 +0900

    Calculate word length and number
    문자열 길이와 숫자를 계산한다.

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Mission {
 	public static void main(String[] avgs){
 		String cmd;
-		String[] arr;
-		String[] word;
-		
+		String[] arr ,word;
+		int len,number;
+		char num;
+
 		Scanner sc = new Scanner(System.in);
 		cmd = sc.nextLine();	
 		
 		arr = cmd.split(" ");	
 		word = String.valueOf(arr[0]).split("");
+		
+		len = String.valueOf(arr[0]).length();		
+		number = Integer.parseInt(arr[1]);	
+		
+		System.out.println(len);		
+		System.out.println(number);
 	}
 }
 
