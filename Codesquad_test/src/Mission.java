2f88b7ae9a3bf0eca7d8fc7e10339fca7863fb98
Tony <dongo824@naver.com>
2020-12-09 14:17:23 +0900

    단어 이동 함수화
    기존 단어 이동 내용을 함수화

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class Mission {
 	public static void main(String[] avgs){
-		String cmd, temp;
-		String[] arr ,word;
-		int len,number;
+		String cmd;
+		String[] arr,word;
+		int len,number,flag = 0;
 		char num;
 
 		Scanner sc = new Scanner(System.in);
@@ -16,32 +16,39 @@ public class Mission {
 		
 		len = String.valueOf(arr[0]).length();		
 		number = Integer.parseInt(arr[1]);	
-	
+		
 		if(number < 0){
 			if((arr[2].equals("L")||arr[2].equals("l"))) arr[2] = "R";
 			else arr[2] = "L";
 		}
 		
-		if(arr[2].equals("L")||arr[2].equals("l")){
+		move(word,arr,Math.abs(number),len);
+	}
+	
+	public static String[] move(String[] word, String[] arr, int number,int len)
+	{
+		String temp;
+		
+		if((arr[2].equals("L")||arr[2].equals("l"))){
 			for(int i=0;i<number;i++){
 				temp = word[0];
 					for(int j=0;j<=len-2;j++){
 						word[j] = word[j+1];
 					}
 				word[len-1] = temp;
-				System.out.println(Arrays.toString(word));
 			}
 		}
-		else if(arr[2].equals("R")||arr[2].equals("r")){
+		else if((arr[2].equals("R")||arr[2].equals("r"))){
 			for(int i=0;i<number;i++){
 				temp = word[len-1];
 					for(int j=len-1;j>=1;j--){
 						word[j] = word[j-1];
 					}
 				word[0] = temp;
-				System.out.println(Arrays.toString(word));
 			}
 		}
+		return word;
 	}
 }
+
 
