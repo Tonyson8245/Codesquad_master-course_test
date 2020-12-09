ca9a2dc5b1809f5efb4e2e3f8258c57ed3a1f3cd
Tony <dongo824@naver.com>
2020-12-09 15:54:56 +0900

    Preview  메소드 생성
    현재 큐브의 모습을 보여주는 view 메소드 생성

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -5,6 +5,17 @@ public class Mission {
 	public static void main(String[] avgs){
 		String [][] cube = {{"R","R","W"},
 						    {"G","C","W"},
-						    {"G","B","B"}};	
-	}	
+						    {"G","B","B"}};
+		preview(cube);
+	}
+	public static void preview(String[][] cube){
+		for (int i = 0; i < cube.length; i++) {
+			for (int j = 0; j < cube[i].length; j++) {
+				System.out.print(cube[i][j] + " "); 
+			}
+			System.out.println();
+		}
+	}
 }
+
+
