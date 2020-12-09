a768772de1a0ae14f5adba9570917548af87c82f
Tony <dongo824@naver.com>
2020-12-09 17:04:10 +0900

    오른쪽 회전 메소드 추가
    오른쪽으로 돌리는 메소드 추가

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -14,7 +14,6 @@ public class Mission {
 			cmd = sc.next();
 			
 			if(cmd.indexOf("Q")>=0){
-				System.out.println("끝");
 				break;
 			}
 		}
@@ -27,6 +26,21 @@ public class Mission {
 			System.out.println();
 		}
 	}
+	public static String[][] move_right(String[][] cube,String line){
+		int row=0,column=0;
+		String temp;
+		
+		if(line.indexOf("U")>=0) row=0;
+		else if (line.indexOf("B")>=0) row=2;
+		
+		temp = cube[row][2];
+		for(int i=2;i>0;i--){
+			cube[row][i] = cube[row][i-1];
+		}
+		cube[row][0] = temp;
+		return cube;
+	}
+	
 }
 
 
