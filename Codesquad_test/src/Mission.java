ca3ecf356bd3fe05e5ca611effc325bd04f60624
Tony <dongo824@naver.com>
2020-12-09 14:54:11 +0900

    큐브 초기화
    초기화

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -1,4 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Mission {
-
+	public static void main(String[] avgs){
+		String [][] cube = {{"R","R","W"},
+						    {"G","C","W"},
+						    {"G","B","B"}};	
+	}	
 }
