0f160eb688a6b8e6c68a85ca7c49a8697f64c2ea
Tony <dongo824@naver.com>
2020-12-09 11:02:42 +0900

    Split command
    명령을 단어, 숫자, 방향으로 나눈다.

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Mission {
 	public static void main(String[] avgs){
 		String cmd;
+		String[] arr;
+		String[] word;
 		
 		Scanner sc = new Scanner(System.in);
 		cmd = sc.nextLine();	
+		
+		arr = cmd.split(" ");	
+		word = String.valueOf(arr[0]).split("");
 	}
 }
 
