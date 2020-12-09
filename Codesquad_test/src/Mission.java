28dca5642566e3af3b54e8366ee9537c1c113e36
Tony <dongo824@naver.com>
2020-12-09 10:54:41 +0900

    Receive command
    명령을 받는다

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 
 public class Mission {
-
+	public static void main(String[] avgs){
+		String cmd;
+		
+		Scanner sc = new Scanner(System.in);
+		cmd = sc.nextLine();	
+	}
 }
+
