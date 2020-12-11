0bc79979b1c5da35c39f004d47a8887a74c8a584
Tony <dongo824@naver.com>
2020-12-11 18:35:52 +0900

    Test_init 메소드 추가
    Test 용 초기화 메소드 추가

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -4,18 +4,36 @@ public class Mission {
 		String cube[][] = new String[3][18];
 		
 		cube = init(cube);
-		preview(cube);
+		preview(cube);			
 	}
 	public static String[][] init(String[][]cube){
 		String color = "B";
+		
 		for(int a=0;a<3;a++){
-			for(int b=0;b<18;b++){
+			for(int b=0;b<18;b++){	
 				if(b>14) color = "R";
 				else if(b>11) color = "Y";
 				else if(b>8) color = "G";
 				else if(b>5) color = "O";
 				else if(b>2) color = "W";
 				else color = "B";
+				cube[a][b] = color;
+			}
+		}
+		return cube;
+	}	
+	public static String[][] test_init(String[][]cube){
+		String color = "B";
+		
+		for(int a=0;a<3;a++){
+			for(int b=0;b<18;b++){	
+				if(b>14) color = "R";
+				else if(b>11) color = "Y";
+				else if(b>8) color = "G";
+				else if(b>5) color = "O";
+				else if(b>2) color = "W";
+				else color = "B";
+				if((a==0)&&(b==0)) color = "K";
 				cube[a][b] = color;
 			}
 		}
@@ -45,3 +63,4 @@ public class Mission {
 		}
 	}
 }
+
