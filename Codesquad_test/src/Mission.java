04814410c40b54186c794c37a064f45f655f04cc
Tony <dongo824@naver.com>
2020-12-12 16:19:33 +0900

    "U" 면 sub_turn_cw/ccw 메소드 추가
    윗면(Upside) 회전 시, 연관되는 면들의 회전

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -5,8 +5,9 @@ public class Mission {
 		cube = init(cube);
 		cube[1][0][0] = "K";
 		
-		cube = main_turn_ccw(cube,1);
-		
+		cube = sub_turn_U_ccw(cube);
+
+
 		view(cube);
 	}
 	public static String[][][] init(String cube[][][]){
@@ -64,6 +65,32 @@ public class Mission {
 		
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++) cube[n][2-j][i] = temp[i][j];
+		}
+		return cube;
+	}
+	public static String[][][] sub_turn_U_cw(String [][][] cube){
+		String temp[][][] = new String[6][3][3];
+		for(int n=0;n<6;n++)for(int a=0;a<3;a++) for(int b=0;b<3;b++) temp[n][a][b] = cube[n][a][b];
+		for(int i=0;i<6;i++){
+			for(int j=0;j<3;j++){
+				if(i==1) cube[4][0][j] = temp[i][0][j];
+				else if(i==2) cube[1][0][j] = temp[i][0][j];
+				else if(i==3) cube[2][0][j] = temp[i][0][j];
+				else if(i==4) cube[3][0][j] = temp[i][0][j];
+			}
+		}
+		return cube;
+	}
+	public static String[][][] sub_turn_U_ccw(String [][][] cube){
+		String temp[][][] = new String[6][3][3];
+		for(int n=0;n<6;n++)for(int a=0;a<3;a++) for(int b=0;b<3;b++) temp[n][a][b] = cube[n][a][b];
+		for(int i=0;i<6;i++){
+			for(int j=0;j<3;j++){
+				if(i==1) cube[2][0][j] = temp[i][0][j];
+				else if(i==2) cube[3][0][j] = temp[i][0][j];
+				else if(i==3) cube[4][0][j] = temp[i][0][j];
+				else if(i==4) cube[1][0][j] = temp[i][0][j];
+			}
 		}
 		return cube;
 	}
