a944d6d6859ef4dc122e198637b76a0c00ed6e2c
Tony <dongo824@naver.com>
2020-12-12 13:29:37 +0900

    "U" 면 sub_turn 메소드 추가
    윗면(Upside) 회전 시, 연관되는 면들의 회전

--- Codesquad_test/src/Mission.java
+++ Codesquad_test/src/Mission.java
@@ -4,8 +4,7 @@ public class Mission {
 		String cube[][] = new String[3][18];
 		
 		cube = test_init(cube);
-		cube = main_turn_ccw(cube,side_num("D"));
-
+		cube = sub_turn_U(cube,"ccw");
 		preview(cube);			
 	}
 	public static String[][] init(String[][]cube){
@@ -35,7 +34,7 @@ public class Mission {
 				else if(b>5) color = "O";
 				else if(b>2) color = "W";
 				else color = "B";
-				if((a==0)&&(b==15)) color = "K";
+				if((a==0)&&(b==3)) color = "K";
 				cube[a][b] = color;
 			}
 		}
@@ -93,11 +92,28 @@ public class Mission {
 			case "U" :n=0; break;
 			case "L" :n=3; break;
 			case "F" :n=6; break;
-			case "B" :n=9; break;
-			case "R" :n=12; break;
+			case "R" :n=9; break;
+			case "B" :n=12; break;
 			case "D" :n=15; break;
 		}
 		return n;
+	}
+	public static String[][] sub_turn_U(String[][] cube,String cw){
+		String temp[][] = new String[3][18];
+		for(int a=0;a<3;a++) for(int b=0;b<18;b++) temp[a][b] = cube[a][b]; 
+		if(cw.equals("cw")){
+			for(int i=14;i>=3;i--){	
+				if(i<6) cube[0][i+9] = temp[0][i];
+				else cube[0][i-3] = temp[0][i];
+			}
+		}
+		else{
+			for(int i=3;i<=14;i++){	
+				if(i>11) cube[0][i-9] = temp[0][i];
+				else cube[0][i+3] = temp[0][i];
+			}
+		}
+		return cube;
 	}
 }
 
